05d7472d9ac7db74b82d3ee048c91ecc8de54924
Bassbulator <belikovm007@gmail.com>
2022-12-25 15:52:41 +0300

    add auxiliary methods.

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from selenium.webdriver.support.select import Select
+
 
 def is_alert_present(wd):
     try:
@@ -16,13 +18,22 @@ class test_add_contact(unittest.TestCase):
 
     def test_add_contact(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.open_homepage(wd)
+        self.login(wd)
+        self.add_new_contact(wd)
+        self.return_homepage(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_homepage(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def add_new_contact(self, wd):
+        # Click by link "add new"
         wd.find_element_by_link_text("add new").click()
+        # fill the form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("Maksim")
@@ -47,20 +58,27 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys("qwerty@mail.ru")
-        #wd.find_element_by_name("bday").click()
-        #Select(wd.find_element_by_name("bday")).select_by_visible_text("9")
-        #wd.find_element_by_xpath("//option[@value='9']").click()
-        #wd.find_element_by_name("bmonth").click()
-        #Select(wd.find_element_by_name("bmonth")).select_by_visible_text("November")
-        #wd.find_element_by_xpath("//option[@value='November']").click()
-        #wd.find_element_by_name("byear").click()
-        #wd.find_element_by_name("byear").clear()
-        #wd.find_element_by_name("byear").send_keys("1999")
+        wd.find_element_by_name("bday").click()
+        Select(wd.find_element_by_xpath("//select[@name='bday']")).select_by_visible_text("9")
+        wd.find_element_by_xpath("//option[@value='9']").click()
+        wd.find_element_by_name("bmonth").click()
+        Select(wd.find_element_by_xpath("//select[@name='bmonth']")).select_by_visible_text("November")
+        wd.find_element_by_xpath("//option[@value='November']").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys("1999")
+        # click by "enter" button
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        wd.find_element_by_link_text("home page").click()
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user").clear()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_homepage(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
 
     def is_element_present(self, how, what):
         try:
