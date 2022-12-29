bc3b0b9ac1da56d706ec7a368b78c41397233b0d
Bassbulator <belikovm007@gmail.com>
2022-12-29 16:05:09 +0300

    Refactoring methods and creating new file Application.

--- test_add_group.py
+++ test_add_group.py
@@ -16,42 +16,45 @@ class test_add_group(unittest.TestCase):
         self.wd.implicitly_wait(60)
     
     def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.init_group_creation(wd)
-        self.fill_group_form(wd, Group(name="qwerty", header="qwerty", footer="qwerty"))
-        self.submit_group_creation(wd)
-        self.return_to_groups_page(wd)
-        self.logout(wd)
+        self.open_home_page()
+        self.login(username="admin", password="secret")
+        self.open_groups_page()
+        self.init_group_creation()
+        self.fill_group_form(Group(name="qwerty", header="qwerty", footer="qwerty"))
+        self.submit_group_creation()
+        self.return_to_groups_page()
+        self.logout()
 
     def test_add_empty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.init_group_creation(wd)
-        self.fill_group_form(wd, Group(name="", header="", footer=""))
-        self.submit_group_creation(wd)
-        self.return_to_groups_page(wd)
-        self.logout(wd)
+        self.open_home_page()
+        self.login(username="admin", password="secret")
+        self.open_groups_page()
+        self.init_group_creation()
+        self.fill_group_form(Group(name="", header="", footer=""))
+        self.submit_group_creation()
+        self.return_to_groups_page()
+        self.logout()
 
-    def open_groups_page(self, wd):
+    def open_groups_page(self):
+        wd = self.wd
         # open groups page
         wd.find_element_by_xpath("//body").click()
         wd.find_element_by_link_text("groups").click()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def return_to_groups_page(self, wd):
+    def return_to_groups_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
-    def submit_group_creation(self, wd):
+    def submit_group_creation(self):
+        wd = self.wd
         wd.find_element_by_name("submit").click()
 
-    def fill_group_form(self, wd, group):
+    def fill_group_form(self, group):
+        wd = self.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -63,10 +66,12 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
 
-    def init_group_creation(self, wd):
+    def init_group_creation(self):
+        wd = self.wd
         wd.find_element_by_name("new").click()
 
-    def login(self, wd, username, password):
+    def login(self, username, password):
+        wd = self.wd
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -74,7 +79,8 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_id("LoginForm").submit()
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
