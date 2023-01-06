52fb731e40e4ed57a48a2525888e0ce4f025c0c6
Bassbulator <belikovm007@gmail.com>
2023-01-06 22:35:36 +0300

    Выделен класс для управления сессиями.

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -12,8 +12,8 @@ def app(request):
 
 def test_add_contact(app):
     app.open_homepage()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_contact(Contact(firstname="Maksim", middlename="Andreevich", lastname="Belikov", nickname="Bassbulator", company="qwerty", address="qwerty", mobile="12345678", email="qwerty@mail.ru"))
     app.return_homepage()
-    app.logout()
+    app.session.logout()
 
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -12,22 +12,22 @@ def app(request):
 
 def test_add_group(app):
     app.open_homepage()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.open_groups_page()
     app.init_group_creation()
     app.fill_group_form(Group(name="qwerty", header="qwerty", footer="qwerty"))
     app.submit_group_creation()
     app.return_to_groups_page()
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
     app.open_homepage()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.open_groups_page()
     app.init_group_creation()
     app.fill_group_form(Group(name="", header="", footer=""))
     app.submit_group_creation()
     app.return_to_groups_page()
-    app.logout()
+    app.session.logout()
 
