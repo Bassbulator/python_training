e6be004717967eb1c495a0e1616c4ab1e99548ba
Bassbulator <belikovm007@gmail.com>
2023-01-09 18:25:47 +0300

    Добавлены тесты по удалению и изменению контакта.

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,6 @@
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.alert import Alert
 
 class ContactHelper:
     def __init__(self, app):
@@ -45,7 +47,45 @@ class ContactHelper:
         # click by "enter" button
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        Alert(wd).accept()
+
+
+    def edit_contact(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//a[@href='edit.php?id=3']").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_xpath("//input[@name='update']").click()
+
+
     def return_homepage(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
-
+        #wd.find_element(By.LINK_TEXT, value="home page")
---
+++ tests/test_delete_contact.py
@@ -0,0 +1,9 @@
+from model.contact import Contact
+
+
+def test_delete_contact(app):
+    app.open_homepage()
+    app.session.login(username="admin", password="secret")
+    app.contact.delete_first_contact()
+    app.session.logout()
+    
