80de8cda2ba034a26939b7221c489ea3e9d60158
Bassbulator <belikovm007@gmail.com>
2023-01-17 17:22:32 +0300

    Доработка вспомогательного класса SessionHelper.

--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -4,3 +4,12 @@ from model.contact import Contact
 def test_edit_contact_firstname(app):
     app.contact.edit_first_contact(Contact(firstname="New name"))
     app.contact.return_homepage()
+
+def test_edit_contact_lastname(app):
+    app.contact.edit_first_contact(Contact(lastname="New lastname"))
+    app.contact.return_homepage()
+
+
+def test_edit_contact_middlename(app):
+    app.contact.edit_first_contact(Contact(middlename="New middlename"))
+    app.contact.return_homepage()
