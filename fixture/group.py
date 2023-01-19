6080898e456a3ddaee2b7e7acf4b44ec4c8be023
Bassbulator <belikovm007@gmail.com>
2023-01-19 17:04:15 +0300

    Добавление условия в тесты test_delete_group.

--- fixture/group.py
+++ fixture/group.py
@@ -7,20 +7,10 @@ class GroupHelper:
         wd = self.app.wd
         # open groups page
         self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # submit_group_creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
 
     def open_groups_page(self):
         wd = self.app.wd
@@ -34,6 +24,7 @@ class GroupHelper:
         self.select_first_group()
         # submit delete
         wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
 
     def edit_first_group(self, new_group_data):
         wd = self.app.wd
@@ -41,11 +32,12 @@ class GroupHelper:
         self.select_first_group()
         # open editing form
         wd.find_element_by_name("edit").click()
-        self.fill_group_from(new_group_data)
+        self.fill_group_form(new_group_data)
         # submit editing form
         wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
 
-    def fill_group_from(self, group):
+    def fill_group_form(self, group):
         wd = self.app.wd
         self.change_field_value("group_name", group.name)
         self.change_field_value("group_header", group.header)
@@ -65,3 +57,8 @@ class GroupHelper:
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
--- tests/test_delete_contact.py
+++ tests/test_delete_contact.py
@@ -2,5 +2,8 @@ from model.contact import Contact
 
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.add_new_contact(Contact(firstname="Maksim", middlename="Andreevich", lastname="Belikov", nickname="Bassbulator",
+                                            company="qwerty", address="qwerty", mobile="12345678", email="qwerty@mail.ru"))
     app.contact.delete_first_contact()
     
