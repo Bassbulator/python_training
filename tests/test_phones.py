b9bf86dc15d0d6b50cedc64533a348a04d96deb0
Bassbulator <belikovm007@gmail.com>
2023-03-12 16:06:12 +0300

    Написание регулярки для телефонов.

--- tests/test_phones.py
+++ tests/test_phones.py
@@ -1,8 +1,13 @@
+import re
+
 
 def test_phones_on_homepage(app):
     contact_from_homepage = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_homepage.homephone == contact_from_edit_page.homephone
-    assert contact_from_homepage.mobilephone == contact_from_edit_page.mobilephone
-    assert contact_from_homepage.workphone == contact_from_edit_page.workphone
-    assert contact_from_homepage.secondaryphone == contact_from_edit_page.secondaryphone
+    assert contact_from_homepage.homephone == clear(contact_from_edit_page.homephone)
+    assert contact_from_homepage.mobilephone == clear(contact_from_edit_page.mobilephone)
+    assert contact_from_homepage.workphone == clear(contact_from_edit_page.workphone)
+    assert contact_from_homepage.secondaryphone == clear(contact_from_edit_page.secondaryphone)
+
+def clear(s):
+    return re.sub("[() -]", "", s)
