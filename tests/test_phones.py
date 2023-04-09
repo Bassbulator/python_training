04160f80804299d7cc0e96d71fd3955c171f24a1
Bassbulator <belikovm007@gmail.com>
2023-04-09 21:42:41 +0300

    fix test_phones.

--- tests/test_phones.py
+++ tests/test_phones.py
@@ -23,5 +23,5 @@ def clear(s):
 def merge_phone_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
-                                filter(lambda x: x is not None, [contact.homephone, contact.workphone,
-                                                                 contact.mobilephone, contact.secondaryphone]))))
+                                filter(lambda x: x is not None, [contact.homephone, contact.mobilephone,
+                                                                 contact.workphone, contact.secondaryphone]))))
