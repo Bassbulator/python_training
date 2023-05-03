3fef4d2509310096d5b3dc74dede9ddbca0f2a83
Bassbulator <belikovm007@gmail.com>
2023-05-03 12:54:07 +0300

    Реализована генерация и чтение тестовых данных из файла.

---
+++ data/contacts.py
@@ -0,0 +1,26 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="firstname1",
+            middlename="middlename1",
+            lastname="lastname1",
+            nickname="nickname1",
+            company="company1",
+            address="address1",
+            email="email1",
+            mobilephone="mobilephone1",
+            homephone="homephone1",
+            secondaryphone="secondaryphone1"),
+    Contact(firstname="firstname2",
+            middlename="middlename2",
+            lastname="lastname2",
+            nickname="nickname2",
+            company="company2",
+            address="address2",
+            email="email2",
+            mobilephone="mobilephone2",
+            homephone="homephone2",
+            secondaryphone="secondaryphone2")
+
+]
---
+++ generator/contact.py
@@ -0,0 +1,54 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import jsonpickle
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+
+n = 5
+f = "data/contacts.json"
+
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname=random_string('firstname', 10),
+                    middlename=random_string('middlename', 10),
+                    lastname=random_string('lastname', 10),
+                    nickname=random_string('nickname', 10),
+                    company=random_string('company', 10),
+                    address=random_string('address', 10),
+                    email=random_string('email', 10),
+                    mobilephone=random_string('mobilephone', 10),
+                    homephone=random_string('home', 10),
+                    secondaryphone=random_string('phone2', 10))
+            for i in range(n)
+            ]
+
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
+
+
+#+ string.punctuation + " "*10
